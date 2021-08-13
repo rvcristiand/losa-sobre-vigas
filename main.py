a0b0c6eda116309f7b9b78d7c6f291b6e7788a3c
rvcristiand <rvcristiand@unal.edu.co>
2021-08-12 21:12:05 -0500

    delete round

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def design(params):
     # datos de entrada
 
     fc = params['fc'] = params.get('fc', 28)
-    fy = 420
+    fy = params['fy'] = params.get('fy', 420)
     L = params['L'] = params.get('L', 10)
     ancho_de_carril = params['ancho_de_carril'] = params.get('ancho_de_carril', 3.6)
     ancho_de_berma = params['ancho_de_berma'] = params.get('ancho_de_berma', 1)
@@ -68,80 +68,80 @@ def design(params):
     recub = params['recub'] = params.get('d',0.06)
     # factor phi para diseño a flexión
     phi = 0.9
-    d = params['d'] = round(h - recub,3)
+    d = params['d'] = (h - recub)
     
 
-    cuantia_kN_pos = params['cuantia_kN_pos'] = round(( 1 - ( 1 - ( 2 * Mupos / ( phi * 1 * d ** 2 * 0.85 * fc * 1000 ) ) ) ** 0.5 ) * 0.85 * fc /  fy, 5)  
+    cuantia_kN_pos = params['cuantia_kN_pos'] = (( 1 - ( 1 - ( 2 * Mupos / ( phi * 1 * d ** 2 * 0.85 * fc * 1000 ) ) ) ** 0.5 ) * 0.85 * fc /  fy)  
     
     #Area de refuerzo de la losa en cm2
-    As_flexion = params['As_flexion'] = round(cuantia_kN_pos * d * 100 * 100, 2)
+    As_flexion = params['As_flexion'] = (cuantia_kN_pos * d * 100 * 100)
 
     #Para barras #8 As = 5.1 cm2
     As_5 = params['As_5'] = 1.99 # definir al inicio
-    No_barras_5_flexion_pos = params['No_barras_5_flexion_pos'] = round(As_flexion / As_5)
+    No_barras_5_flexion_pos = params['No_barras_5_flexion_pos'] = (As_flexion / As_5)
 
     #Espaciamiento Armadura a flexión para momentos positivo
-    espac_arm_prin_flexion_pos = params['espac_arm_prin_flexion_pos'] = round (100 / No_barras_5_flexion_pos)
+    espac_arm_prin_flexion_pos = params['espac_arm_prin_flexion_pos'] = (100 / No_barras_5_flexion_pos)
 
     # Revisión del factor phi = 0.9 para el diseño a flexión para momentos positivo
 
-    a_f_pos = params['a_f_pos'] = round(cuantia_kN_pos * d * fy /( 0.85 * fc), 4)
+    a_f_pos = params['a_f_pos'] = (cuantia_kN_pos * d * fy /( 0.85 * fc))
 
     #profundidad eje neutro
 
     betha_1 = 0.85
-    c_f_pos = params['c_f_pos'] = round(a_f_pos / betha_1 , 4) 
+    c_f_pos = params['c_f_pos'] = (a_f_pos / betha_1 ) 
 
     #Relacion de  deformaciones en la sección de concreto reforzado
 
-    defor_total_pos = params['defor_total_pos'] = round((d - c_f_pos) * (0.003 / c_f_pos), 4)  
+    defor_total_pos = params['defor_total_pos'] = ((d - c_f_pos) * (0.003 / c_f_pos))  
 
     # Momento ultimo negativo
     Muneg = params['Muneg'] = factor_mod_carga * ( 1.25 * MDC + 1.5 * MDW + 1.75 * MLLIMneg )
    
-    cuantia_kN_neg =  params['cuantia_kN_neg'] = round(( 1 - ( 1 - ( 2 * Muneg / ( phi * 1 * d ** 2 * 0.85 * fc * 1000 ) ) ) ** 0.5 ) * 0.85 * fc /  fy, 5)  
+    cuantia_kN_neg =  params['cuantia_kN_neg'] = (( 1 - ( 1 - ( 2 * Muneg / ( phi * 1 * d ** 2 * 0.85 * fc * 1000 ) ) ) ** 0.5 ) * 0.85 * fc /  fy)  
 
     #Area de refuerzo de la losa en cm2
-    As_flexion_neg = params['As_flexion_neg'] = round(cuantia_kN_neg * d * 100 * 100, 2)
+    As_flexion_neg = params['As_flexion_neg'] = (cuantia_kN_neg * d * 100 * 100)
 
     #Para barras #8 As = 5.1 cm2
 
-    No_barras_5_flexion_neg = params['No_barras_5_flexion_neg'] = round(As_flexion_neg / As_5)
+    No_barras_5_flexion_neg = params['No_barras_5_flexion_neg'] = (As_flexion_neg / As_5)
     
     #Espaciamiento Armadura a flexión para momentos positivo
-    espac_arm_prin_flexion_neg = params['espac_arm_prin_flexion_neg'] = round (100 / No_barras_5_flexion_neg)
+    espac_arm_prin_flexion_neg = params['espac_arm_prin_flexion_neg'] =  (100 / No_barras_5_flexion_neg)
     
 
     # Revisión del factor phi = 0.9 para el diseño a flexión para momentos positivo
 
-    a_f_neg = params['a_f_neg'] = round(cuantia_kN_neg * d * fy /( 0.85 * fc), 4)
+    a_f_neg = params['a_f_neg'] = (cuantia_kN_neg * d * fy /( 0.85 * fc))
     
     #profundidad eje neutro
 
     betha_1 = 0.85
-    c_f_neg = params['c_f_neg'] = round(a_f_neg / betha_1 , 4) 
+    c_f_neg = params['c_f_neg'] = (a_f_neg / betha_1) 
 
     #Relacion de  deformaciones en la sección de concreto reforzado
 
-    defor_total_neg = params['defor_total_neg'] = round((d - c_f_neg) * (0.003 / c_f_neg), 4)  
+    defor_total_neg = params['defor_total_neg'] = ((d - c_f_neg) * (0.003 / c_f_neg))  
     
     #Armadura dedistribución para losas con armadura principal paralela a la dirección del trafico (9.7.3.2)
 
-    Armadura_de_distribucion = params['Armadura_de_distribucion'] = round(3840 / (S_entre_vigas*1000) ** 0.5 / 100, 2 )
+    Armadura_de_distribucion = params['Armadura_de_distribucion'] = (3840 / (S_entre_vigas*1000) ** 0.5 / 100 )
     
     if Armadura_de_distribucion > 0.67 :
         Armadura_de_distribucion = 0.67
 
     As_4 = 1.29
-    As_Armadura_de_distribucion = params['As_Armadura_de_distribucion'] = round ( Armadura_de_distribucion * As_flexion , 2)
+    As_Armadura_de_distribucion = params['As_Armadura_de_distribucion'] =  ( Armadura_de_distribucion * As_flexion)
 
-    No_barras_4_dist = params['No_barras_4_dist'] =  round( As_Armadura_de_distribucion / As_4 )
+    No_barras_4_dist = params['No_barras_4_dist'] = ( As_Armadura_de_distribucion / As_4 )
     
-    espac_arm_dist = params['espac_arm_dist'] =  round( 100 / No_barras_4_dist )
+    espac_arm_dist = params['espac_arm_dist'] =  ( 100 / No_barras_4_dist )
 
     #Armadura minima
     #Modulo de rotura del concreto
-    fr = params['fr'] = round(0.62 * fc ** 0.5, 2)
+    fr = params['fr'] = (0.62 * fc ** 0.5)
     
     # factor de variacion de l afisuracion por flexion 5.7.3.3.2 
     gamma_1 = params['gamma_1'] = params.get('gamma_1',1.6)
@@ -152,9 +152,9 @@ def design(params):
 
     #Modulo elastico de la seccion
 
-    Sc = params['Sc'] = round(1 * h ** 2 / 6, 4 )
+    Sc = params['Sc'] = (1 * h ** 2 / 6 )
     
-    Mcr = params['Mcr'] = round (gamma_1 ** 2 * gamma_3 * fr * Sc * 1000)
+    Mcr = params['Mcr'] = (gamma_1 ** 2 * gamma_3 * fr * Sc * 1000)
     
     if Mcr > Muneg :
         print ('No cumple Armadura mínima')
@@ -172,7 +172,7 @@ def design(params):
     params['d_cf'] = d_cf
 
     #De acuerdo con el Art 5.7.3.4, el coeficiente beta s 
-    beta_s = params['beta_s'] = round (1 + d_cf / (0.7 * (h - d_cf)), 3)
+    beta_s = params['beta_s'] =  (1 + d_cf / (0.7 * (h - d_cf)))
 
     #Calculo de fss: Esfuerzo actuante a tracción en el acero para estado limite de servicio I
     
@@ -181,7 +181,7 @@ def design(params):
 
     # Modulo de elasticidad del concreto - densidad normal MPa
 
-    E_concreto = params['E_concreto'] = round(0.043 * 2350 ** 1.5 * (fc) ** 0.5, 2)
+    E_concreto = params['E_concreto'] = (0.043 * 2350 ** 1.5 * (fc) ** 0.5)
 
     # Modulo de elasticidad del acero MPa
     
@@ -189,24 +189,24 @@ def design(params):
 
     #Relación modular
 
-    rel_mod = params['rel_mod'] = round(E_acero / E_concreto)
+    rel_mod = params['rel_mod'] = (E_acero / E_concreto)
 
     #Momento de primer orden de la sección  fisurada, de 1m de ancho, con respecto al eje neutro
 
     #Tomando momentos con respecto al eje neutro de la sección:
 
-    X_cf = params['X_cf'] = round (( -(2 * rel_mod * As_flexion * 10 ** -4) + ((2 * rel_mod * As_flexion * 10 ** -4 ) ** 2 - (4 * 1 * -2 * rel_mod * As_flexion * 10 ** -4 * d )) ** 0.5 ) / (2 * 1), 2)
+    X_cf = params['X_cf'] =  (( -(2 * rel_mod * As_flexion * 10 ** -4) + ((2 * rel_mod * As_flexion * 10 ** -4 ) ** 2 - (4 * 1 * -2 * rel_mod * As_flexion * 10 ** -4 * d )) ** 0.5 ) / (2 * 1))
 
     # Momento de inercia de la seccion fisurada
     d_ = params['d_'] = (h - d_c)
-    I_c = params['I_c'] = round(X_cf ** 3 / 3 + rel_mod * As_flexion * 10 ** -4 * (d_ - X_cf) ** 2, 8)
+    I_c = params['I_c'] = (X_cf ** 3 / 3 + rel_mod * As_flexion * 10 ** -4 * (d_ - X_cf) ** 2)
     
 
-    fss = params['fss'] = round (rel_mod * Msi * (d_ - X_cf) / I_c / 1000, 2)
+    fss = params['fss'] =  (rel_mod * Msi * (d_ - X_cf) / I_c / 1000)
 
     # Reemplazando en la ecuación 5.7.3.4.1
     
-    espac_control_fisuracion = params['espac_control_fisuracion'] = round((123000 * gamma_e / (beta_s * fss) ) - (2 * d_cf * 1000))
+    espac_control_fisuracion = params['espac_control_fisuracion'] = ((123000 * gamma_e / (beta_s * fss) ) - (2 * d_cf * 1000))
 
     if espac_control_fisuracion / 100 > espac_arm_prin_flexion_pos :
         print ('No cumple control de fisuración')
@@ -228,7 +228,7 @@ def design(params):
         print ('No cumple espaciamineto minimo 5.10.3')
 
     #Armadura por retraccion de fraguado y temperatura
-    As_retytemp = round(750 * ancho_total * h / (2 * (ancho_total + h) * fy) * 1000)
+    As_retytemp = (750 * ancho_total * h / (2 * (ancho_total + h) * fy) * 1000)
 
     if 234 > As_retytemp :
         As_retytemp = 234
@@ -236,11 +236,11 @@ def design(params):
         print ('No cumple Retraccion y Temperatura') 
     params['As_retytemp'] = As_retytemp
     As_3 = 0.71
-    No_barras_3_retytemp = params ['No_barras_3_retytemp'] = round (As_retytemp /100 / As_3, 2) 
+    No_barras_3_retytemp = params ['No_barras_3_retytemp'] =  (As_retytemp /100 / As_3) 
 
-    espa_arm_retytemp = params['espa_arm_retytemp'] = round(100 / No_barras_3_retytemp)
+    espa_arm_retytemp = params['espa_arm_retytemp'] = (100 / No_barras_3_retytemp)
     
-    h3 = params['h3'] = round(3 * h)
+    h3 = params['h3'] = (3 * h)
     
 
     #Franja exterior del puente 
@@ -248,45 +248,45 @@ def design(params):
     dist_aplicacion_P = params['dist_aplicacion_P'] = params.get('dist_aplicacion_P', 0.3) # Según 3.6.1.3.1
     dist_ancho_franja_equiv = params['dist_ancho_franja_equiv'] = dist_eje_viga_borde - ancho_inferior_de_bordillo - dist_aplicacion_P
     
-    E_borde = params['E_borde'] = round(1.14 + 0.833 * dist_ancho_franja_equiv , 2)
+    E_borde = params['E_borde'] = (1.14 + 0.833 * dist_ancho_franja_equiv)
 
     #Avaluo de cargas permanentes para la franja exterior
-    DC_bordillo = params['DC_bordillo'] = round(altura_de_bordillo * ancho_inferior_de_bordillo * pespecifico_concreto , 2)
+    DC_bordillo = params['DC_bordillo'] = (altura_de_bordillo * ancho_inferior_de_bordillo * pespecifico_concreto)
 
-    DC_ext = params['DC_ext'] = round(pespecifico_concreto * (h * dist_eje_viga_borde) , 2)
+    DC_ext = params['DC_ext'] = (pespecifico_concreto * (h * dist_eje_viga_borde))
 
-    DC_barrera = params['DC_barrera'] = params.get('DC_barrera',round(0.07 * 9.81 , 2))
+    DC_barrera = params['DC_barrera'] = params.get('DC_barrera',(0.07 * 9.81))
 
-    DW_ext = params['DW_ext'] = round(pespecifico_carpeta_asfaltica * espesor_de_carpeta_asfaltica  , 2) 
+    DW_ext = params['DW_ext'] = (pespecifico_carpeta_asfaltica * espesor_de_carpeta_asfaltica) 
 
     #Momentos maximos debidos a cargas permanentes
 
-    MDC_ext = params['MDC_ext'] = round( DC_bordillo * (dist_eje_viga_borde - ancho_inferior_de_bordillo/2) + DC_ext * dist_eje_viga_borde/2 + DC_barrera * (dist_eje_viga_borde - ancho_inferior_de_bordillo), 2)
+    MDC_ext = params['MDC_ext'] = ( DC_bordillo * (dist_eje_viga_borde - ancho_inferior_de_bordillo/2) + DC_ext * dist_eje_viga_borde/2 + DC_barrera * (dist_eje_viga_borde - ancho_inferior_de_bordillo))
 
-    MDW_ext = params['MDW_ext'] = round(DW_ext * (dist_eje_viga_borde - ancho_inferior_de_bordillo) ** 2 / 2 , 2)
+    MDW_ext = params['MDW_ext'] = (DW_ext * (dist_eje_viga_borde - ancho_inferior_de_bordillo) ** 2 / 2 )
     
     
     #Momento debido a carga viva
     # Debido a que sobre la franja exterior actua una rueda y no un eje, se dividen las solicitaciones en 2 y en el ancho de la franja exterior
-    MLLIM_ext = params['MLLIM_ext'] = round(80 * dist_ancho_franja_equiv * 1.33/ E_borde, 2)
+    MLLIM_ext = params['MLLIM_ext'] = (80 * dist_ancho_franja_equiv * 1.33/ E_borde)
 
     #Determinacion de armadura a flexion de franja exterior
     
-    Mu_ext = params['Mu_ext'] = round((1.25 * MDC_ext + 1.5 * MDW_ext + 1.75 * MLLIM_ext), 2)
+    Mu_ext = params['Mu_ext'] = ((1.25 * MDC_ext + 1.5 * MDW_ext + 1.75 * MLLIM_ext))
     
     #cuantia franja exterior 
     
-    cuantia_ext = params['cuantia_ext'] = round(( 1 - ( 1 - ( 2 * Mu_ext / ( phi * 1 * d ** 2 * 0.85 * fc * 1000 ) ) ) ** 0.5 ) * 0.85 * fc /  fy, 4)
+    cuantia_ext = params['cuantia_ext'] = (( 1 - ( 1 - ( 2 * Mu_ext / ( phi * 1 * d ** 2 * 0.85 * fc * 1000 ) ) ) ** 0.5 ) * 0.85 * fc /  fy)
 
-    As_flexion_ext = params['As_flexion_ext'] = round(cuantia_ext * d * 100 * 100)
+    As_flexion_ext = params['As_flexion_ext'] = (cuantia_ext * d * 100 * 100)
 
     #Numero de barras #8 flexion franja exterior
 
-    No_barras_5_flexion_ext = params['No_barras_5_flexion_ext'] = round (As_flexion_ext / As_5) 
+    No_barras_5_flexion_ext = params['No_barras_5_flexion_ext'] = (As_flexion_ext / As_5) 
 
     #Separacion de barras a flexion franja exterior
 
-    espac_arm_prin_flexion_ext = params['espac_arm_prin_flexion_ext'] = round (100 / No_barras_5_flexion_ext)
+    espac_arm_prin_flexion_ext = params['espac_arm_prin_flexion_ext'] = (100 / No_barras_5_flexion_ext)
 
     return params
 
